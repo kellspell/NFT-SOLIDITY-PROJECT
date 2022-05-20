539905b324cbec7a52f02609a4e2661975f4d060
Kellspell <kskeelmadrid3@gmail.com>
2022-05-20 10:52:47 +0100

    small NFT project

---
+++ metadata/sample_metadata.py
@@ -0,0 +1,6 @@
+metadata_template = {
+    "name": "RIDEN",
+    "description": "Human (Formerly God)",
+    "image": "https://ipfs.io/ipfs/QmTjs4EVxWKHzVdCmX1naGtE5Lp7ruDbsjaJ1F8VSBtjsk?filename=Ra.png",
+    "attributes": [{"Fighting Styles": "Nan Chuan", "Jujutsu" "Judo" "Tae Kwon Do": 100}],
+}
